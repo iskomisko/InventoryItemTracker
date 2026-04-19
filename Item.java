308f2b8bfb95880581668c76ba581d357cd2d51e
iskomisko <iscomorenoo@gmail.com>
2026-04-19 16:49:02 +0600

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,54 @@
+
+public class Item {
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+
+//    constructure
+    public Item(int id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+//    getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+//    setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+//    методы вывода тут
+
+    public void display() {
+        System.out.println("ID: " + id +
+                ", Name: " + name +
+                ", Quantity: " + quantity +
+                ", Price: " + price);
+    }
+}
